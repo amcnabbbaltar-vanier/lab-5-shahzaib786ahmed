fa125c5fd569f6af14ee245822b5eb8329b7aa77
shahzaib786ahmed <ahmedshahzaib203@gmail.com>
2025-10-01 10:50:04 -0400

    Configuration

--- Assets/Scripts/PlayerAnimationManager.cs
+++ Assets/Scripts/PlayerAnimationManager.cs
@@ -20,7 +20,7 @@ public class PlayerAnimatorController : MonoBehaviour
 
     public void Update()
     {
-        animator.SetFloat("CharacterSpeed", rb.linearVelocity.magnitude);
+        animator.SetFloat("CharacterSpeed", rb.velocity.magnitude);
         animator.SetBool("IsGrounded", movement.IsGrounded);
        
         if (Input.GetButtonUp("Fire1"))
